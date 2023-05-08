d917242d392b9172508929fbb7402af1b19562f0
Наима <113019779+Naimabilal19@users.noreply.github.com>
2023-05-08 12:45:32 +0300

    Add files via upload

---
+++ sync/sync/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace sync
+{
+    public partial class Form1 : Form
+    {
+        string name, path;
+        Mutex m = new Mutex();
+        public Form1()
+        {
+            InitializeComponent();
+            path = textBox1.Text;
+        }
+
+        public void NameFile()
+        {
+            m.WaitOne();
+            name = Path.GetFileName(textBox1.Text);
+            listBox1.Items.Add("File name: " + name);
+            m.ReleaseMutex();
+        }
+        public void PathFile()
+        {
+            m.WaitOne();
+            listBox1.Items.Add("Path file: " + textBox1.Text);
+            m.WaitOne();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread th = new Thread(new ThreadStart(NameFile));
+            th.Start();
+            Thread th1 = new Thread(new ThreadStart(PathFile));
+            th1.Start();
+        }
+    }
+}
